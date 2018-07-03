acf63f71c4e9942309c60b42106ef39634940c69
Freitas <freitas.adf@hotmail.com>
2018-07-03 16:28:45 +0200

    Dag 1§
    movement och camerafollow
    Enkel movement som wasd och hoppa

---
+++ Sommarprojekt2018/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Av Andreas de Freitas
+public class CameraFollow : MonoBehaviour
+{
+    #region Variabler
+    [SerializeField]
+    GameObject _player;
+
+    Vector3 _offset;
+    #endregion
+
+    #region Metoder
+    void Start()
+    {
+        _offset = transform.position - _player.transform.position; //Räknar ut offset värdet genom att ta differensen mellan spelarensposition och kameransposition
+    }
+
+    void LateUpdate() //Sätter kameransposition till samma som spelarensposition fast med uträknad offset
+    {
+        transform.position = _player.transform.position + _offset;
+    }
+    #endregion
+}
---
+++ Sommarprojekt2018/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Av Andreas de Freitas
+public class PlayerMovement : MonoBehaviour
+{
+    #region Variabler
+    [SerializeField]
+    float _speed, _sprintSpeed, _jumpSpeed, _gravity;
+
+    Vector3 _moveDirection = Vector3.zero;
+
+    CharacterController _characterController;
+
+
+    #endregion
+
+    #region Metoder
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+
+        if (_characterController.isGrounded) //Ifall spelaren befinner sig på marken
+        {
+            _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            _moveDirection = transform.TransformDirection(_moveDirection);
+            _moveDirection *= _speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                _moveDirection.y = _jumpSpeed;
+            }
+        }
+        _moveDirection.x = Input.GetAxis("Horizontal") * _speed; //Tillåter spelaren strafea i x-led i luften
+        _moveDirection.z = Input.GetAxis("Vertical") * _speed; //Tillåter spelaren strafea i z-led i luften
+
+        _moveDirection.y -= _gravity * Time.deltaTime;
+        _characterController.Move(_moveDirection * Time.deltaTime);
+    }
+
+    #endregion
+}
