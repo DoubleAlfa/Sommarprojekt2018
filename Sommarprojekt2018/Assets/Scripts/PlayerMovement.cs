ced1494447e1eac1dcf11d228add36f16c1d8de4
Freitas <freitas.adf@hotmail.com>
2018-07-03 12:29:25 +0200

    Playermovement
    börjar med playermovement

---
+++ Sommarprojekt2018/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Av Andreas de Freitas
+public class PlayerMovement : MonoBehaviour {
+    
+    #region Variabler
+    
+    #endregion
+}
