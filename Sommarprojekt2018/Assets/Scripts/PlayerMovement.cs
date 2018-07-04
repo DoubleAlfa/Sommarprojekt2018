eff0915e158942e1d5c2ef56f5281bae712b7b37
Freitas <freitas.adf@hotmail.com>
2018-07-04 13:19:47 +0200

    Movement
    WASD
    Sprint
    hoppa
    dubbelhoppa
    ISDONEYOO

--- Sommarprojekt2018/Assets/Scripts/PlayerMovement.cs
+++ Sommarprojekt2018/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     #region Variabler
+
     [SerializeField]
     float _speed, _sprintSpeed, _jumpSpeed, _gravity;
+
+    float _normalSpeed; //Håller koll på ursprungshastigheten av spelaren
+
+    bool _doubleJump = false;
+
     Vector3 _moveDirection = Vector3.zero;
 
     CharacterController _characterController;
@@ -14,35 +20,45 @@ public class PlayerMovement : MonoBehaviour
     #endregion
 
     #region Metoder
+
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
+        _normalSpeed = _speed;
     }
 
     void Update()
     {
-        print(_speed);
+        _speed = _normalSpeed;
+
         if (_characterController.isGrounded) //Ifall spelaren befinner sig på marken
         {
             _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             _moveDirection = transform.TransformDirection(_moveDirection);
             _moveDirection *= _speed;
 
-            if (Input.GetButton("Jump"))
+            if (Input.GetButtonDown("Jump")) //Hoppa
             {
                 _moveDirection.y = _jumpSpeed;
-                //Fixa dubbelhopp
+                _doubleJump = true;
             }
 
-            if (Input.GetKeyDown(KeyCode.LeftShift))
+            if (Input.GetButton("Sprint")) //Springa
             {
-                //sprint
+                _speed = _sprintSpeed;
             }
-
         }
+        else
+        {
+            if (Input.GetButtonDown("Jump") && _doubleJump) //Dubbelhopp
+            {
+                _moveDirection.y = _jumpSpeed;
+                _doubleJump = false;
+            }
+        }
+
         _moveDirection.x = Input.GetAxis("Horizontal") * _speed; //Tillåter spelaren strafea i x-led i luften
         _moveDirection.z = Input.GetAxis("Vertical") * _speed; //Tillåter spelaren strafea i z-led i luften
-
         _moveDirection.y -= _gravity * Time.deltaTime;
         _characterController.Move(_moveDirection * Time.deltaTime);
     }
